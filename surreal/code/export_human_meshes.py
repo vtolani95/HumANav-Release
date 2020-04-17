8eee190d5f9cedc543bdf10aa9866acc4a45eef4
Varun Tolani <vatolani@gmail.com>
2020-04-17 14:54:55 -0700

    clean up export human meshes script

--- surreal/code/export_human_meshes.py
+++ surreal/code/export_human_meshes.py
@@ -1,14 +1,19 @@
+#### The following script will save meshes in .obj format of a human
+#### with a specified body shape and gender. Human poses come from
+#### mocap data from real human subjects. This script is heavily
+#### based on code from the SURREAL dataset: https://github.com/gulvarol/surreal
+### This particular file is based on main_part1.py
+#### https://github.com/gulvarol/surreal/blob/master/datageneration/main_part1.py
+
+
 import sys
 import os
 import random
 import math
 import bpy
 import numpy as np
-from os import getenv
-from os import remove
 from os.path import join, dirname, realpath, exists
 from mathutils import Matrix, Vector, Quaternion, Euler
-from glob import glob
 from random import choice
 from pickle import load
 from bpy_extras.object_utils import world_to_camera_view as world2cam
@@ -40,170 +45,6 @@ part_match = {'root':'root', 'bone_00':'Pelvis', 'bone_01':'L_Hip', 'bone_02':'R
               'bone_19':'R_Elbow', 'bone_20':'L_Wrist', 'bone_21':'R_Wrist', 'bone_22':'L_Hand', 'bone_23':'R_Hand'}
 
 part2num = {part:(ipart+1) for ipart,part in enumerate(sorted_parts)}
-
-# create one material per part as defined in a pickle with the segmentation
-# this is useful to render the segmentation in a material pass
-def create_segmentation(ob, params):
-    materials = {}
-    vgroups = {}
-    with open('pkl/segm_per_v_overlap.pkl', 'rb') as f:
-        vsegm = load(f)
-    bpy.ops.object.material_slot_remove()
-    parts = sorted(vsegm.keys())
-    vertices = np.concatenate([vsegm[key] for key in vsegm.keys()], axis=0)
-    for part in parts:
-        vs = vsegm[part]
-        vgroups[part] = ob.vertex_groups.new(part)
-        vgroups[part].add(vs, 1.0, 'ADD')
-        bpy.ops.object.vertex_group_set_active(group=part)
-        materials[part] = bpy.data.materials['Material'].copy()
-        materials[part].pass_index = part2num[part]
-        bpy.ops.object.material_slot_add()
-        ob.material_slots[-1].material = materials[part]
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.select_all(action='DESELECT')
-        bpy.ops.object.vertex_group_select()
-        bpy.ops.object.material_slot_assign()
-        bpy.ops.object.mode_set(mode='OBJECT')
-    return(materials)
-
-# create the different passes that we render
-def create_composite_nodes(tree, params, img=None, idx=0):
-    res_paths = {k:join(params['tmp_path'], '%05d_%s'%(idx, k)) for k in params['output_types'] if params['output_types'][k]}
-    
-    # clear default nodes
-    for n in tree.nodes:
-        tree.nodes.remove(n)
-
-    # create node for foreground image
-    layers = tree.nodes.new('CompositorNodeRLayers')
-    layers.location = -300, 400
-
-    # create node for background image
-    bg_im = tree.nodes.new('CompositorNodeImage')
-    bg_im.location = -300, 30
-    if img is not None:
-        bg_im.image = img
-
-    if(params['output_types']['vblur']):
-    # create node for computing vector blur (approximate motion blur)
-        vblur = tree.nodes.new('CompositorNodeVecBlur')
-        vblur.factor = params['vblur_factor']
-        vblur.location = 240, 400
-
-        # create node for saving output of vector blurred image 
-        vblur_out = tree.nodes.new('CompositorNodeOutputFile')
-        vblur_out.format.file_format = 'PNG'
-        vblur_out.base_path = res_paths['vblur']
-        vblur_out.location = 460, 460
-
-    # create node for mixing foreground and background images 
-    mix = tree.nodes.new('CompositorNodeMixRGB')
-    mix.location = 40, 30
-    mix.use_alpha = True
-
-    # create node for the final output 
-    composite_out = tree.nodes.new('CompositorNodeComposite')
-    composite_out.location = 240, 30
-
-    # create node for saving depth
-    if(params['output_types']['depth']):
-        depth_out = tree.nodes.new('CompositorNodeOutputFile')
-        depth_out.location = 40, 700
-        depth_out.format.file_format = 'OPEN_EXR'
-        depth_out.base_path = res_paths['depth']
-
-    # create node for saving normals
-    if(params['output_types']['normal']):
-        normal_out = tree.nodes.new('CompositorNodeOutputFile')
-        normal_out.location = 40, 600
-        normal_out.format.file_format = 'OPEN_EXR'
-        normal_out.base_path = res_paths['normal']
-
-    # create node for saving foreground image
-    if(params['output_types']['fg']):
-        fg_out = tree.nodes.new('CompositorNodeOutputFile')
-        fg_out.location = 170, 600
-        fg_out.format.file_format = 'PNG'
-        fg_out.base_path = res_paths['fg']
-
-    # create node for saving ground truth flow 
-    if(params['output_types']['gtflow']):
-        gtflow_out = tree.nodes.new('CompositorNodeOutputFile')
-        gtflow_out.location = 40, 500
-        gtflow_out.format.file_format = 'OPEN_EXR'
-        gtflow_out.base_path = res_paths['gtflow']
-
-    # create node for saving segmentation
-    if(params['output_types']['segm']):
-        segm_out = tree.nodes.new('CompositorNodeOutputFile')
-        segm_out.location = 40, 400
-        segm_out.format.file_format = 'OPEN_EXR'
-        segm_out.base_path = res_paths['segm']
-    
-    # merge fg and bg images
-    tree.links.new(bg_im.outputs[0], mix.inputs[1])
-    tree.links.new(layers.outputs['Image'], mix.inputs[2])
-    
-    if(params['output_types']['vblur']):
-        tree.links.new(mix.outputs[0], vblur.inputs[0])                # apply vector blur on the bg+fg image,
-        tree.links.new(layers.outputs['Z'], vblur.inputs[1])           #   using depth,
-        tree.links.new(layers.outputs['Speed'], vblur.inputs[2])       #   and flow.
-        tree.links.new(vblur.outputs[0], vblur_out.inputs[0])          # save vblurred output
-    
-    tree.links.new(mix.outputs[0], composite_out.inputs[0])            # bg+fg image
-    if(params['output_types']['fg']):
-        tree.links.new(layers.outputs['Image'], fg_out.inputs[0])      # save fg
-    if(params['output_types']['depth']):    
-        tree.links.new(layers.outputs['Z'], depth_out.inputs[0])       # save depth
-    if(params['output_types']['normal']):
-        tree.links.new(layers.outputs['Normal'], normal_out.inputs[0]) # save normal
-    if(params['output_types']['gtflow']):
-        tree.links.new(layers.outputs['Speed'], gtflow_out.inputs[0])  # save ground truth flow
-    if(params['output_types']['segm']):
-        tree.links.new(layers.outputs['IndexMA'], segm_out.inputs[0])  # save segmentation
-
-    return(res_paths)
-
-# creation of the spherical harmonics material, using an OSL script
-def create_sh_material(tree, sh_path, img=None):
-    # clear default nodes
-    for n in tree.nodes:
-        tree.nodes.remove(n)
-
-    uv = tree.nodes.new('ShaderNodeTexCoord')
-    uv.location = -800, 400
-
-    uv_xform = tree.nodes.new('ShaderNodeVectorMath')
-    uv_xform.location = -600, 400
-    uv_xform.inputs[1].default_value = (0, 0, 1)
-    uv_xform.operation = 'AVERAGE'
-
-    uv_im = tree.nodes.new('ShaderNodeTexImage')
-    uv_im.location = -400, 400
-    if img is not None:
-        uv_im.image = img
-
-    rgb = tree.nodes.new('ShaderNodeRGB')
-    rgb.location = -400, 200
-
-    script = tree.nodes.new('ShaderNodeScript')
-    script.location = -230, 400
-    script.mode = 'EXTERNAL'
-    script.filepath = sh_path #'spher_harm/sh.osl' #using the same file from multiple jobs causes white texture
-    script.update()
-
-    # the emission node makes it independent of the scene lighting
-    emission = tree.nodes.new('ShaderNodeEmission')
-    emission.location = -60, 400
-
-    mat_out = tree.nodes.new('ShaderNodeOutputMaterial')
-    mat_out.location = 110, 400
-    
-    tree.links.new(uv.outputs[2], uv_im.inputs[0])
-    tree.links.new(uv_im.outputs[0], script.inputs[0])
-    tree.links.new(script.outputs[0], emission.inputs[0])
-    tree.links.new(emission.outputs[0], mat_out.inputs[0])
 
 # computes rotation matrix through Rodrigues formula as in cv2.Rodrigues
 def Rodrigues(rotvec):
@@ -241,13 +82,9 @@ def init_scene(scene, params, gender='female'):
                                  (0., -1, 0., -1.0),
                                  (-1., 0., 0., 0.),
                                  (0.0, 0.0, 0.0, 1.0)))
-    #cam_ob.data.angle = math.radians(40)
-    #cam_ob.data.lens =  60
-    #cam_ob.data.clip_start = 0.1
-    #cam_ob.data.sensor_width = 32
 
-    # Note: Varun- Using the SBPD Renderer Settings Here
-    # SBPD Renderer Settings
+    # The following camera settings are irrelevant
+    # as we are saving actual meshes (not images)
     # 90 FOV, z_near = .01, focal length = .01
     # (or image plane =.02x.02 [2*.01 since 45, 45, 90 triangle])
     cam_ob.rotation_mode = 'XYZ'
@@ -435,16 +272,13 @@ def main():
     stride = args.stride
     gender = args.gender
     body_shape_idx = args.body_shape_idx
-    #idx = 1
-    #ishape = 0
-    #stride = 50
-    
+
     log_message("input idx: %d" % idx)
     log_message("input ishape: %d" % ishape)
     log_message("input stride: %d" % stride)
     log_message("gender: %s" % gender)
     log_message("body_shape_idx: %d" % body_shape_idx)
-    
+
     if idx == None:
         exit(1)
     if ishape == None:
@@ -462,7 +296,6 @@ def main():
     else:
         assert(gender in ['male', 'female'])
     
-    # import idx info (name, split)
     idx_info = load(open("pkl/idx_info.pickle", 'rb'))
 
     # get runpass
@@ -482,47 +315,38 @@ def main():
     
     smpl_data_folder = params['smpl_data_folder']
     smpl_data_filename = params['smpl_data_filename']
-    #bg_path = params['bg_path']
-    resy = params['resy']
-    resx = params['resx']
     clothing_option = params['clothing_option'] # grey, nongrey or all
     tmp_path = params['tmp_path']
     output_path = params['output_path']
     output_types = params['output_types']
     stepsize = params['stepsize']
     clipsize = params['clipsize']
-    #openexr_py2_path = params['openexr_py2_path']
 
     # compute number of cuts
-    #import pdb; pdb.set_trace()
     nb_ishape = max(1, int(np.ceil((idx_info['nb_frames'] - (clipsize - stride))/stride)))
     log_message("Max ishape: %d" % (nb_ishape - 1))
-    
+
     if ishape == None:
         exit(1)
-    
+
     assert(ishape < nb_ishape)
-    
+
     # name is set given idx
     name = idx_info['name']
     output_path = join(output_path, 'run%d' % runpass, name.replace(" ", ""))
     params['output_path'] = output_path
     tmp_path = join(tmp_path, 'run%d_%s_c%04d' % (runpass, name.replace(" ", ""), (ishape + 1)))
     params['tmp_path'] = tmp_path
-    
+
     # check if already computed
     #  + clean up existing tmp folders if any
     if exists(tmp_path) and tmp_path != "" and tmp_path != "/":
         os.system('rm -rf %s' % tmp_path)
-    rgb_vid_filename = "%s_c%04d.mp4" % (join(output_path, name.replace(' ', '')), (ishape + 1))
-    #if os.path.isfile(rgb_vid_filename):
-    #    log_message("ALREADY COMPUTED - existing: %s" % rgb_vid_filename)
-    #    return 0
-    
+
     # create tmp directory
     if not exists(tmp_path):
         mkdir_safe(tmp_path)
-    
+
     # >> don't use random generator before this point <<
 
     # initialize RNG with seeds from sequence id
@@ -540,30 +364,19 @@ def main():
     log_message("Setup Blender")
 
     # create copy-spher.harm. directory if not exists
-    sh_dir = join(tmp_path, 'spher_harm')
-    if not exists(sh_dir):
-        mkdir_safe(sh_dir)
-    sh_dst = join(sh_dir, 'sh_%02d_%05d.osl' % (runpass, idx))
-    os.system('cp spher_harm/sh.osl %s' % sh_dst)
+    #sh_dir = join(tmp_path, 'spher_harm')
+    #if not exists(sh_dir):
+    #    mkdir_safe(sh_dir)
+    #sh_dst = join(sh_dir, 'sh_%02d_%05d.osl' % (runpass, idx))
+    #os.system('cp spher_harm/sh.osl %s' % sh_dst)
 
-    # TODO: VARUN: Choose gender
     genders = {0: 'female', 1: 'male'}
-    # pick random gender
-    #gender = choice(genders)
-    #gender = 'male'
 
     scene = bpy.data.scenes['Scene']
     scene.render.engine = 'CYCLES'
     bpy.data.materials['Material'].use_nodes = True
     scene.cycles.shading_system = True
     scene.use_nodes = True
-
-    #log_message("Listing background images")
-    #bg_names = join(bg_path, '%s_img.txt' % idx_info['use_split'])
-    #nh_txt_paths = []
-    #with open(bg_names) as f:
-    #    for line in f:
-    #        nh_txt_paths.append(join(bg_path, line))
 
     # grab clothing names
     log_message("clothing: %s" % clothing_option)
@@ -575,55 +388,23 @@ def main():
         txt_paths = [k for k in txt_paths if 'nongrey' in k]
     elif clothing_option == 'grey':
         txt_paths = [k for k in txt_paths if 'nongrey' not in k]
-    
+
     # random clothing texture
     cloth_img_name = choice(txt_paths)
     cloth_img_name = join(smpl_data_folder, cloth_img_name)
-    cloth_img = bpy.data.images.load(cloth_img_name)
-
-    # random background
-    #bg_img_name = choice(nh_txt_paths)[:-1]
-    #bg_img = bpy.data.images.load(bg_img_name)
-
-    log_message("Loading parts segmentation")
-    beta_stds = np.load(join(smpl_data_folder, ('%s_beta_stds.npy' % gender)))
-    
-    #log_message("Building materials tree")
-    #mat_tree = bpy.data.materials['Material'].node_tree
-    #create_sh_material(mat_tree, sh_dst, cloth_img)
-    #res_paths = create_composite_nodes(scene.node_tree, params, img=bg_img, idx=idx)
 
     log_message("Loading smpl data")
     smpl_data = np.load(join(smpl_data_folder, smpl_data_filename))
-    
+
     log_message("Initializing scene")
     #TODO: Varun. Setting the Camera Distance from the Human Manually Here
     camera_distance = 4.0
-    #camera_distance = np.random.normal(8.0, 1)
     params['camera_distance'] = camera_distance
     ob, obname, arm_ob, cam_ob = init_scene(scene, params, gender)
 
     setState0()
     ob.select = True
     bpy.context.scene.objects.active = ob
-    segmented_materials = True #True: 0-24, False: expected to have 0-1 bg/fg
-    
-    log_message("Creating materials segmentation")
-    # create material segmentation
-    if False:
-    #if segmented_materials:
-        materials = create_segmentation(ob, params)
-        prob_dressed = {'leftLeg':.5, 'leftArm':.9, 'leftHandIndex1':.01,
-                        'rightShoulder':.8, 'rightHand':.01, 'neck':.01,
-                        'rightToeBase':.9, 'leftShoulder':.8, 'leftToeBase':.9,
-                        'rightForeArm':.5, 'leftHand':.01, 'spine':.9,
-                        'leftFoot':.9, 'leftUpLeg':.9, 'rightUpLeg':.9,
-                        'rightFoot':.9, 'head':.01, 'leftForeArm':.5,
-                        'rightArm':.5, 'spine1':.9, 'hips':.9,
-                        'rightHandIndex1':.01, 'spine2':.9, 'rightLeg':.5}
-    else:
-        materials = {'FullBody': bpy.data.materials['Material']}
-        prob_dressed = {'FullBody': .6}
 
     orig_pelvis_loc = (arm_ob.matrix_world.copy() * arm_ob.pose.bones[obname+'_Pelvis'].head.copy()) - Vector((-1., 1., 1.))
     orig_cam_loc = cam_ob.location.copy()
@@ -643,17 +424,7 @@ def main():
     # Note Varun T. Force the train split
     fshapes = fshapes[:int(nb_fshapes*0.8)]
     
-    #if idx_info['use_split'] == 'train':
-    #    fshapes = fshapes[:int(nb_fshapes*0.8)]
-    #elif idx_info['use_split'] == 'test':
-    #    fshapes = fshapes[int(nb_fshapes*0.8):]
-    
-    # pick random real body shape
-    # TODO: VARUN: Choose body shape
-    #body_shape_idx = choice(np.r_[:len(fshapes)])
-    #shape = choice(fshapes) #+random_shape(.5) can add noise
     shape = fshapes[body_shape_idx]
-    #shape = random_shape(3.) # random body shape
     
     # example shapes
     #shape = np.zeros(10) #average
@@ -671,16 +442,6 @@ def main():
     if not exists(output_path):
         mkdir_safe(output_path)
 
-    # spherical harmonics material needs a script to be loaded and compiled
-    #scs = []
-    #for mname, material in materials.items():
-    #    scs.append(material.node_tree.nodes['Script'])
-    #    scs[-1].filepath = sh_dst
-    #    scs[-1].update()
-
-    rgb_dirname = name.replace(" ", "") + '_c%04d.mp4' % (ishape + 1)
-    rgb_path = join(tmp_path, rgb_dirname)
-
     data = cmu_parms[name]
     
     fbegin = ishape*stepsize*stride
@@ -692,7 +453,6 @@ def main():
 
     # force recomputation of joint angles unless shape is all zeros
     curr_shape = np.zeros_like(shape)
-    nframes = len(data['poses'][::stepsize])
 
     matfile_info = join(output_path, name.replace(" ", "") + "_c%04d_info.mat" % (ishape+1))
     log_message('Working on %s' % matfile_info)
@@ -714,7 +474,7 @@ def main():
     dict_info['zrot'] = np.empty(N, dtype='float32')
     dict_info['camDist'] = camera_distance
     dict_info['stride'] = stride
-    
+
     # Note: Added by Varun T. for centering the human.
     dict_info['rightFootPos'] = np.empty((3, N), dtype='float32')
     dict_info['rightToePos'] = np.empty((3, N), dtype='float32')
@@ -734,7 +494,6 @@ def main():
     get_real_frame = lambda ifr: ifr
     random_zrot = 0
     reset_loc = False
-    batch_it = 0
     curr_shape = reset_joint_positions(orig_trans, shape, ob, arm_ob, obname, scene,
                                        cam_ob, smpl_data['regression_verts'], smpl_data['joint_regressor'])
     random_zrot = 2*np.pi*np.random.rand()
@@ -753,12 +512,6 @@ def main():
     pose_ishape_stride_str = 'pose_{:d}_ishape_{:d}_stride_{:d}'.format(idx, ishape, stride)
     body_shape_str = 'body_shape_{:d}'.format(body_shape_idx)
     gender_str = gender
-    #human_data_output_folder = os.path.join('visual_mpc_human_meshes_with_velocity_information',
-    #                                        'pose_{:d}_ishape_{:d}_stride_{:d}'.format(idx, ishape, stride),
-    #                                        'body_shape_{:d}'.format(body_shape_idx),
-    #                                        gender)
-    #if not os.path.exists(human_data_output_folder):
-    #    os.makedirs(human_data_output_folder)
 
     # create a keyframe animation with pose, translation, blendshapes and camera motion
     # LOOP TO CREATE 3D ANIMATION
@@ -770,7 +523,7 @@ def main():
 
         # apply the translation, pose and shape to the character
         apply_trans_pose_shape(Vector(trans), pose, shape, ob, arm_ob, obname, scene, cam_ob, get_real_frame(seq_frame))
-        
+
         dict_info['shape'][:, iframe] = shape[:ndofs]
         dict_info['pose'][:, iframe] = pose
         dict_info['gender'][iframe] = list(genders)[list(genders.values()).index(gender)]
@@ -817,14 +570,11 @@ def main():
                 with open(centeringFile, 'wb') as f:
                     pickle.dump(centering_data, f)
 
-                # Note; Varun Exporting Human Mesh Here for Use in SBPD
+                # Exporting Human Mesh Here for Use in HumANav
                 meshFile = os.path.join(human_data_output_folder, 'human_mesh_{:d}.obj'.format(seq_frame))
                 bpy.ops.export_scene.obj(filepath=meshFile,
-                                         keep_vertex_order=True, group_by_object=True)#,
-                                        #axis_forward='Z', axis_up='-Y')
+                                         keep_vertex_order=True, group_by_object=True)
     os._exit(0)
-    #import pdb; pdb.set_trace()
-    #pass
 
 def compute_velocity_folder(human_speed, vs, velocity_folders, vmax, vmin):
     if human_speed > vmax or human_speed < vmin:
