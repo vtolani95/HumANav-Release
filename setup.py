6c197484cc0b978f54d84ed666f9e12ab4f754f4
vtolani95 <vatolani@gmail.com>
2020-04-29 08:32:40 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@ from setuptools import setup
 from setuptools import find_packages
 
 setup(name='humanav',
-  version='0.0.1',
+  version='0.0',
   author='Varun Tolani',
   author_email='vtolani@eecs.berkeley.edu',
   description='Human Active Navigation Dataset',
   packages=find_packages(),
   license='UC Berkeley',
-  install_requires=[]  
-#install_requires=['PyOpenGL', 'pyopengl-accelerate', 'numpy',
-  #  'opencv-python', 'pyassimp', 'pytest', 'ipython', 'scikit-image',
-  #  'yattag']
+  install_requires=[]
 )
